9249e8eb22e9572eb3b10cd3ecd41285326d8c9e
Lynn McQueen <lynn.edward.mcqueen@gmail.com>
2023-10-05 04:38:30 -0600

    Adds the server signup capability, along with the User model

--- server/app.py
+++ server/app.py
@@ -3,16 +3,61 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response, session
 from flask_restful import Resource
+
 
 # Local imports
 from config import app, db, api
+from models import User
 # Add your model imports
 
+def get_property_val_from_user_dict(value, user_dict ):
+    if value in user_dict:
+        return user_dict[value]
+    return None
 
 # Views go here!
+class Signup(Resource):
+    def post(self):
+        new_user_params = request.get_json()
 
+        new_user = get_property_val_from_user_dict("username", new_user_params)
+
+        new_user = User(
+            username = get_property_val_from_user_dict("username", new_user_params ),
+            firstName = get_property_val_from_user_dict("firstName", new_user_params ),
+            lastName = get_property_val_from_user_dict("lastName", new_user_params ),
+            email = get_property_val_from_user_dict("email", new_user_params ),
+            phone = get_property_val_from_user_dict("phone", new_user_params ),
+            address1 = get_property_val_from_user_dict("address1", new_user_params ),
+            address2 = get_property_val_from_user_dict("address2", new_user_params ),
+            city = get_property_val_from_user_dict("city", new_user_params ),
+            state = get_property_val_from_user_dict("state", new_user_params ),
+            zip = get_property_val_from_user_dict("zip", new_user_params )    
+        )
+        new_user.password_hash = get_property_val_from_user_dict("password", new_user_params )
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            new_user_added = True
+        except:
+            new_user_added = False
+        
+        # Set up the session cookie.  We're in luck!
+        if new_user_added:
+            session["user_id"] = new_user.id
+            response = make_response(
+                new_user.to_dict(),
+                201
+            )
+        else:
+             response = {}, 422
+        
+        return response
+
+api.add_resource(Signup, '/signup', endpoint='signup')
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
--- server/models.py
+++ server/models.py
@@ -1,6 +1,53 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from config import db
+
+from config import db,bcrypt
 
 # Models go here!
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable = False, unique = True)
+    firstName = db.Column(db.String, nullable = False)
+    lastName = db.Column(db.String, nullable = False)
+    email = db.Column(db.String, nullable = False)
+    phone = db.Column(db.String, nullable = False)
+    address1 = db.Column(db.String, nullable = False)
+    address2 = db.Column(db.String)
+    city = db.Column(db.String, nullable = False)
+    state = db.Column(db.String, nullable = False)
+    zip = db.Column(db.String, nullable = False)
+    _password_hash = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User {self.username} {self.id}>'
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("password hash access restricted")
+        return self._password_hash
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+    
+    # this was created sinse the to_dict() method is not working. 
+#    def get_user_dictionary(self):
+#        return {
+#            "username": self.username,
+#            "image_url": self.image_url,
+#            "bio": self.bio,
+#            "id": self.id
+#        }
+
+
